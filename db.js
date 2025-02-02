caf92b750965257420b09e116b65e4356c020374
juyeon320 <145083246+juyeon320@users.noreply.github.com>
2025-02-02 19:42:13 +0900

    a

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
-//db.js
-
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config(); // .env 파일 로드
 
 const pool = mysql.createPool({
-  host: 'database-1.cvy64gkcwjwg.ap-northeast-2.rds.amazonaws.com', // MariaDB 서버 (로컬)
-  user: 'admin', // 사용자명
-  password: 'duswndl2871', // 비밀번호
-  database: 'database-1', // 데이터베이스명
-  port: 13306, // 기본 MariaDB 포트
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
